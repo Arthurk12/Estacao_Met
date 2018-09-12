738e5a920fae14534573f68515a60a09a7864b04
Arthur <arthur_bockmann@hotmail.com>
2018-09-11 22:20:26 -0300

    1_cod

--- cod_estacao.ino
+++ cod_estacao.ino
@@ -1,103 +1,156 @@
-#include <DHT.h>                                   //Biblioteca do Sensor de Umidade
-#include <Ethernet.h>                             //Biblioteca do Ethernet Shield
-#include <SPI.h>                                 //Biblioteca da Interface Periferica Serial
-#include <DallasTemperature.h>                  //Biblioteca de Temperaturas do Sensor DS18B20
-#include <OneWire.h>                           //Biblioteca de Comunicação do Sensor DS18B20
-
-byte mac [ ] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };      //Define o MAC ADDRESS do Ethernet Shield
-IPAddress ip(192,168,0,122);                               //Define IP
-IPAddress gateway(192,168,0,1);                           //Define GATEWAY
-IPAddress subnet(255, 255, 255, 0);                      //Define MASCARA DE REDE
-
-EthernetServer server (81);                   //Inicia o SERVIDOR WEB na PORTA (81)
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <DHT.h>
+#include <Ethernet.h>
 
 
+//ETHERNET************************
+byte mac [ ] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192,168,0,122);
+IPAddress gateway(192,168,0,1);
+IPAddress subnet(255, 255, 255, 0);
 
+EthernetServer server (81);
+//********************************
 
-// Pin definitions
-#define LED 9                    //pino digial 9 (LED)
-#define DHTPIN 6                   //pino digital 6(DHT)
-#define ONE_WIRE_BUS 5         //Pino digital 4(termometro)
-#define Hall sensor 2         //Pino digital 2(anemometro)
+//TERMOMETRO**********************
+#define ONE_WIRE_BUS 4
 
-// Constants definitions
-#define pi 3.14159265           // Numero pi
-#define period 5000               // Tempo de medida(miliseconds)
-#define delaytime 1000             // Time between samples (miliseconds)
-#define radius 147      // Raio do anemometro(mm)
+OneWire oneWire(ONE_WIRE_BUS);
 
-
-//SENSOR DE UMIDADE DEFINITIONS***********************************
-#define DHTTYPE DHT21         //sensor DHT 21
-DHT dht(DHTPIN, DHTTYPE);
-//****************************************************************
-
-//DECLARAÇÃO DE FUNCOES
-void windvelocity();
-void RPMcalc();
-void SpeedWind();
-void addcount();
-void calcumidade();
-
-//DECLARAÇÃO DE VARIAVEIS
-float tempMin = 999;
-float tempMax = 0;
-
-float tempC=0;
-
-unsigned int counter = 0; // magnet counter for sensor
-unsigned int RPM = 0;          // Revolutions per minute
-float speedwind = 0;         // Wind speed (m/s)
-float windspeed = 0;           // Wind speed (km/h)
-float maxspeed=0;             //maior velocidade do vento[Km/h]
-float umidade=0;              //umidade
-float umidademax=0;           //umidade maxima
-float umidademin=0;           //umidade minima
-
-OneWire oneWire (ONE_WIRE_BUS);
-
-DallasTemperature sensors (&oneWire);
+DallasTemperature sensors(&oneWire);
 DeviceAddress sensor1;
+
+float tempMin=999;
+float tempMax=0;
+float tempC=0;
+//*******************************
+
+//DHT21**************************
+#define DHTPIN 6
+#define DHTTYPE DHT21
+
+DHT dht(DHTPIN, DHTTYPE);
+
+float hmin=999;
+float hmax=0;
+float h=0;
+//*******************************
+
+//ANEMOMETRO*********************
+# define Hall sensor 2
+// Constants definitions
+#define pi 3.14159265
+#define period 5000
+#define radius 147
+
+// Variable definitions
+unsigned short int counter = 0;
+unsigned short int RPM = 0;
+float speedwind = 0;
+float speedwindmax=0;
+float medspeed=0;
+//*******************************
+
+//LED****************************
+#define LED 9
+//*******************************
 
 
 void setup() {
-
   Ethernet.begin(mac, ip, gateway, subnet);
   server.begin();
-
-//****************************************************************
-//TERMOMETRO******************************************************
-  //Start serial 
-  Serial.begin(9600);                                     //CASO FALTE ESPAÇO, REVER ESTA PARTE DO CODIGO.
   sensors.begin();
-  // Localiza e mostra enderecos dos sensores
-  Serial.println("Localizando sensores DS18B20...");
-  Serial.print("Foram encontrados ");
-  Serial.print(sensors.getDeviceCount(), DEC);
-  Serial.println(" sensores.");
-  if (!sensors.getAddress(sensor1, 0)) 
-     Serial.println("Sensores nao encontrados !"); 
-//****************************************************************
-//LED*************************************************************
-  pinMode(LED, OUTPUT);
-//****************************************************************
-//ANEMOMETRO******************************************************
-// Set the pins
-  pinMode(Hall sensor, INPUT);
-  digitalWrite(Hall sensor, HIGH);     //internall pull-up active
-//****************************************************************
+  sensors.getAddress(sensor1, 0);
+  dht.begin();
+  pinMode(2, INPUT);
+  digitalWrite(2, HIGH);
+  Serial.begin(9600);
+  pinMode(9,OUTPUT);
+  
+}
 
-//SENSOR DE UMIDADE***********************************************
-dht.begin();
-//****************************************************************
+void TEMPERATURA(){
+  
+  
+  //Le informaçoes do sensor
+  sensors.requestTemperatures();
+  tempC = sensors.getTempC(sensor1);
+  // Atualiza temperaturas minima e maxima
+  if (tempC < tempMin){
+    tempMin = tempC;
+  }
+  if (tempC > tempMax){
+    tempMax = tempC;
+  }
+
+}
+
+void UMIDADE(){
+  h = dht.readHumidity();
+
+
+  if(h < hmin){
+    
+    hmin = h;
+    
+  }else if(h > hmax){
+
+    hmax = h;
+  }
+}
+
+void addcount(){
+  counter++;
+} 
+
+void windvelocity(){
+  
+  counter = 0;  
+  attachInterrupt(0, addcount, RISING);
+  unsigned long millis();       
+  long startTime = millis();
+  while(millis() < startTime + period) {
+  }
+}
+
+void RPMcalc(){
+  RPM=((counter)*60)/(period/1000);
+}
+
+void SpeedWind(){
+  speedwind = (((4 * pi * radius * RPM)/60) / 1000)*3.6;
+}
+
+void ANEMOMETRO(){
+  windvelocity();
+  RPMcalc();
+  //print km/h  
+  SpeedWind();
+
+  if(speedwind > speedwindmax)
+    speedwindmax = speedwind;
+}
+
+void MEDIAVEL(){
+
+  if(medspeed==0){
+    medspeed=speedwind;
+  }else{
+    medspeed+=speedwind;
+    medspeed=medspeed/2;
+  }
 }
 
 void loop() {
-//****************************************************************
-    digitalWrite(LED, HIGH);
-//****************************************************************
-
-
+  digitalWrite(LED, HIGH);
+  TEMPERATURA();
+  UMIDADE();
+  digitalWrite(LED, LOW);
+  ANEMOMETRO();
+  MEDIAVEL();
+  digitalWrite(LED, HIGH);
+  
   EthernetClient client = server.available();
   if (client) {
     Serial.println("New Client");
@@ -105,55 +158,82 @@ void loop() {
     while (client.connected()) {
       char c = client.read();
       Serial.write(c);
-      if (c == 'n' && currentLineIsBlank) {
+      if (c == 'n' && currentLineIsBlank){
         client.println("HTTP/1.1 200 OK");
         client.println("Content-Type: text/html");
         client.println("Connection: close");
-        client.println("Refresh: 2");       //RECARREGA A PAGINA A CADA 2 SEGUNDOS
+        client.println("Refresh: 2");
         client.println();
         client.println("<!DOCTYPE html>");
         client.println("<html><head>");
-        //client.println("<meta http-equiv="content-type" content="text/html; charset=Windows-1252">");
         client.println("<title>");
-        client.println("ESTAÇÃO METEOROLÓGICA");          //Titulo da Pagina (no navegador)
+        client.println("ESTA&Ccedil;&Atilde;O METEOROL&Oacute;GICA"); 
         client.println("</title>");
-        client.println("</head>");
-        client.println("<body>");
-        client.println("<h1> ESTAÇÃO METEOROLÓGICA </h1>");     //Titulo da Pagina (na pagina)
+        client.println("</head><body>");
+        client.println("<center><h1><b><u> ESTA&Ccedil;&Atilde;O METEOROL&Oacute;GICA </u></b></h1></center>");
         client.println("<p></p>");
-        client.println("<table>");
-        client.println("<tbody><tr>");
-        client.println("<td>Sensor:</td>");                     //Nome dos Sensores
-        client.println("<p></p>");
-        client.println("<td>Status:</td>");                    //Status dos Sensores
-        client.println("<td>Informação:</td>");               //Informação dos Sensores
-        client.println("<td>Maxima:</td>");                  //Maxima Registrada Pelos Sensores
-        client.println("<td>Mínima:</td>");                 //Minima Registrada Pelos Sensores
+        client.println("<table width=20% border=40 bordercolor = #4169E1 bgcolor = #DCDCDC cellspacing=20 align=center><tbody>");
+        client.println("<tr>");
+        client.println("<td><center><b>Sensor:</b></td></center>");
+        client.println("<td><center><b>Status:</b></td></center>");
+        client.println("<td><center><b>Atual:</b></td></center>");
+        client.println("<td><center><b>M&aacute;xima:</b></td></center>");
+        client.println("<td><center><b>M&iacute;nima:</b></td></center>");
+        client.println("<td><center><b>M&eacute;dia:</b></td></center>");
         client.println("</tr>");
         client.println("<tr>");
-        client.println("<td>Temperatura: (<code>°</code>C)</td>");
-        client.println("<td>Ligado</td>");
-        client.println("<td>tempC</td>");           //Variavel do sensor
-        client.println("<td>tempMax</td)");           //Variavel de Maxima Registrada do Sensor
-        client.println("<td>tempMin</td>");          //Variavel de Minima Registrada do Sensor
+        client.println("<td><center><b>Temperatura:(&#176;C)</b></td></center>");
+        client.println("<td><center>Ligado</td></center>");
+        client.println("<td><center>");
+        client.println(tempC);
+        client.println("</td></center>");
+        client.println("<td><center>");
+        client.println(tempMax);
+        client.println("</td></center>");
+        client.println("<td><center>");
+        client.println(tempMin);
+        client.println("</td></center>");
+        client.println("<td><center>");
+        client.println("--------");
+        client.println("</td></center>");
         client.println("</tr>");
         client.println("<tr>");
-        client.println("<td>Velocidade do Vento: (Km/h)</td>");
-        client.println("<td>Ligado</td>");
-        client.println("<td>windspeed</td>");         //Variavel do Sensor
-        client.println("<td>maxspeed</td>");          //Variavel de Maxima Registrada do Sensor
-        client.println("<td>  --  </td>");        //Variavel de Minima Registrada do Sensor
+        client.println("<td><center><b>Velocidade do Vento: (Km/h)</b></td></center>");
+        client.println("<td><center>Ligado</td></center>");
+        client.println("<td><center>");
+        client.println(speedwind);
+        client.println("</td></center>");
+        client.println("<td><center>");
+        client.println(speedwindmax);
+        client.println("</td></center>");
+        client.println("<td><center>");
+        client.println("--------");
+        client.println("</td></center>");
+        client.println("<td><center>");
+        client.println(medspeed);
+        client.println("</td></center>");
         client.println("</tr>");
         client.println("<tr>");
-        client.println("<td>Umidade: (%)</td>");
-        client.println("<td>Ligado</td>");
-        client.println("<td>umidade</td>");       //Variavel do Sensor
-        client.println("<td>umidademax</td>");       //Variavel de Maxima Registrada do Sensor
-        client.println("<td>umidademin</td>");      //Variavel de Minima Registrada do Sensor
-        client.println("</tr>");
-        client.println("</tbody></table>");
-        client.println("</body></html>");
-        
+        client.println("<td><center><b>Umidade: (%)</b></td></center>");
+        client.println("<td><center>Ligado</td></center>");
+        client.println("<td><center>");   
+        client.println(h);
+        client.println("</td></center>");
+        client.println("<td><center>");   
+        client.println(hmax);
+        client.println("</td></center>");
+        client.println("<td><center>");   
+        client.println(hmin);
+        client.println("</td></center>");
+        client.println("<td><center>");
+        client.println("--------");
+        client.println("</td></center>");
+        client.println("</tr></tbody></table></body>");
+        client.println("<h5><center>Todos Direitos Reservados Copyright &copy; Arthur B&ouml;ckmann Grossi &and; Mateus Scalco Rampon</center></h5>");
+        client.println("</html>");
+        break;
+
+      }
       if (c == 'n') {
         currentLineIsBlank = true;
       }
@@ -164,85 +244,9 @@ void loop() {
   }
   delay (1);
   client.stop();
-}
-
-//****************************************************************
-  windvelocity();
-  RPMcalc();
-//****************************************************************
   digitalWrite(LED, LOW);
-//****************************************************************
-//VELOCIDADE DO VENTO ATUAL
-  SpeedWind();
-  Serial.print(speedwind);
-//****************************************************************
-//MAIOR VELOCIDADE DO VENTO;
-  Serial.print(maxspeed);
-//****************************************************************
-  digitalWrite(LED, HIGH);
-//****************************************************************
-
-//****************************************************************
-  sensors.requestTemperatures();
-  tempC = sensors.getTempC(sensor1);
-  if(tempC>tempmax){
-    tempmax=tempC;
-  }else if(tempC<tempmin){
-    tempmin=tempC;
-  }
-  Serial.print(tempC);
-//****************************************************************
-
-//****************************************************************
-  digitalWrite(LED, LOW);
-//****************************************************************
-
-//UMIDADE*********************************************************
-  calcumidade();
-  Serial.print(umidade);
-
-//****************************************************************
 }
 
-  void windvelocity(){
-    speedwind = 0;
-    
-    counter = 0;  
-    attachInterrupt(0, addcount, RISING);
-    unsigned long millis();       
-    long startTime = millis();
-    while(millis() < startTime + period) {
-    }
-}
-  
-  
-  
-  void RPMcalc(){
-    RPM=((counter)*60)/(period/1000);  //RPM
-}
-  
-  
-  
-  void SpeedWind(){
-    speedwind = (((4 * pi * radius * RPM)/60) / 1000)*3.6;  // VELOCIDADE DO VENTO EM KM/H
-    
-    if(speedwind>maxspeed)      //REGISTRA A MAIOR VELOCIDADE
-      maxspeed=speedwind;
-}
-  
-  
-  void addcount(){
-    counter++;
-} 
 
-  void calcumidade(){
-    //CHAMA A LEITURA DA UMIDADE
-    umidade = dht.readHumidity(); 
 
-       if(umidade>umidademax){
-        umidademax=umidade;
 
-       }else if(umidade<umidademin){
-        umidademin=umidade;   
-       }
-}
